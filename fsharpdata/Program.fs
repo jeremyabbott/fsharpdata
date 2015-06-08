a4323dae87531472e137fe2cf2321bb97f066d37
Jeremy Abbott <jeremymabbott@gmail.com>
2015-06-08 18:15:23 -0500

    initial commit

---
+++ fsharpdata/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open FSharp.Data
+
+type Issue = JsonProvider<"https://api.github.com/repos/fsharp/FSharp.Data/issues">
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
